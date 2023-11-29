7fcdab832c604010b0b894a98fbbca85b3a2271c
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-11-28 20:09:58 -0800

    homepage creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomepageLayout from "scenes/HomepageLayout";
 
 function App() {
-  return <div className="Pokedex"></div>;
+  return (
+    <div className="Pokedex">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomepageLayout />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/scenes/HomepageLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomepageLayout = () => {
+  return (
+    <div>
+      <h1>Pokedex Main Intro Page</h1>
+    </div>
+  );
+};
+
+export default HomepageLayout;
