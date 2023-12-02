ba3539db08d00dea049e03d19e2deba7da9984db
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-12-02 01:40:33 -0800

    update with slider

--- client/src/scenes/PokemonScrollDisplayScreen.jsx
+++ client/src/scenes/PokemonScrollDisplayScreen.jsx
@@ -3,6 +3,8 @@ import axios from "utils/axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./PokemonScrollDisplayScreen.css";
+import { Box } from "@mui/material";
 
 const PokemonScrollDisplayScreen = () => {
   const [pokemonData, setPokemonData] = useState([]);
@@ -21,24 +23,59 @@ const PokemonScrollDisplayScreen = () => {
     fetchData();
   }, []);
 
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 3,
+  //   vertical: true,
+  //   verticalSwiping: true,
+  //   swipeToSlide: true,
+  //   beforeChange: function (currentSlide, nextSlide) {
+  //     console.log("before change", currentSlide, nextSlide);
+  //   },
+  //   afterChange: function (currentSlide) {
+  //     console.log("after change", currentSlide);
+  //   },
+  // };
+
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    vertical: true,
-    verticalSwiping: true,
-    swipeToSlide: true,
-    beforeChange: function (currentSlide, nextSlide) {
-      console.log("before change", currentSlide, nextSlide);
-    },
-    afterChange: function (currentSlide) {
-      console.log("after change", currentSlide);
-    },
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: (dots) => (
+      <Box
+        style={{
+          backgroundColor: "#ddd",
+          borderRadius: "10px",
+          padding: "10px",
+          margin: "-100px",
+          justifyContent: "center",
+          marginRight: "auto",
+          marginLeft: "auto",
+          width: "70%",
+          alignItems: "center",
+        }}
+      >
+        <ul style={{ margin: "5px" }}> {dots} </ul>
+      </Box>
+    ),
+    customPaging: (i) => (
+      <div
+        style={{
+          width: "30px",
+          color: "blue",
+          border: "1px blue solid",
+        }}
+      >
+        {i + 1}
+      </div>
+    ),
   };
-
   return (
-    <div>
+    <div className="pageLayout">
       <Slider {...settings}>
         {pokemonData.map((x, index) => (
           <div key={index}>
